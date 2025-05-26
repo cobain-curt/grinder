70d9f5d0dfe21a4f0cf1c866a0a2c87c4b4e77b6
Curt Cobain <aktansan1234@gmail.com>
2025-05-26 12:47:42 +0500

    switch to gunicorn for production

--- app.py
+++ app.py
@@ -9,6 +9,3 @@ def index():
     # Список графиков в static/
     charts = [f for f in os.listdir('static') if f.endswith(('.png', '.jpg', '.jpeg'))]
     return render_template('index.html', charts=charts)
-
-if __name__ == '__main__':
-    app.run(debug=True)
